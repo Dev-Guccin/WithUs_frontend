f4a46ad662a1e804d3bf3a9c1fcd119ef424f62b
wodhks08 <ghks2047@naver.com>
2021-04-30 10:58:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'; // import 로 useState 를 불러온다!
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [test, settest ] = useState();
+  
+  useEffect(() => {
+    // 브라우저 API를 이용하여 문서 타이틀을 업데이트합니다.
+    axios.get('http://localhost:3001/test',{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      console.log(response)
+      settest(response.data)
+    });
+    });  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {test}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
